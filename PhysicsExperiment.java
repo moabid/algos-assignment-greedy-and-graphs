b98081dd5c3be8b140e0865ee8cd5bdf83ac105e
moabid <moabid@yahoo.com>
2019-05-10 18:28:48 -0400

    Update PhysicsExperiment.java

--- PhysicsExperiment.java
+++ PhysicsExperiment.java
@@ -1,7 +1,7 @@
 /**
  * Physics Experiment
  * Author: Mohammed Abid and Carolyn Yao
- * Does this compile or finish running within 5 seconds? Y/N
+ * Does this compile or finish running within 5 seconds? YES
  */
 
 /**
